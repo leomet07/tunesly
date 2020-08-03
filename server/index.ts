a4881af5a1f2511ce82e835d9585018c39839ee4
leomet07 <leomet07@gmail.com>
2020-08-02 21:19:35 -0400

    Added helmet + morgan

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 // tslint:disable:no-console
 import express from "express";
 import * as dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 
 const app = express();
+
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
 	res.send("Hello world from ts!");
@@ -12,7 +17,7 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-// start the Express server
+// Start the Express server
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}`);
 });
