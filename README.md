acba487cfc38a4b84680c46179710a10ecfb46ab
leomet07 <leomet07@gmail.com>
2020-08-04 19:46:56 -0400

    Added basic audio playback.

---
+++ README.md
@@ -0,0 +1,30 @@
+# WebMusicMaker-Editor 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](#)
+
+> A Web Music Generator
+
+## Install
+
+```sh
+Follow the instructions in the server/ and client/ folders. 
+```
+
+## Usage
+
+```sh
+ Follow the instructions in the server/ and client/ folders.
+```
+
+## Author
+
+👤 **leomet07, shamar, ysowasted, TheRisingDeveloper**
+
+* Github: [@ leomet07, shamar, ysowasted, TheRisingDeveloper](https://github.com/ leomet07, shamar, ysowasted, TheRisingDeveloper)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,68 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import * as Tone from "tone";
+// import soundfile from "../public/music.mp3";
+// const playsound = async (tSc: any) => {
+// 	// create HTMLAudioElement
+// 	let audio = new Audio(soundfile);
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+// 	if (tSc === timerStates.COMPLETE)
+// 		return (
+// 			// play HTMLAudioElement
+// 			audio.play()
+// 		);
+// };
+
+class App extends React.Component {
+	componentDidMount() {}
+	async play() {
+		try {
+			console.log("clicked");
+
+			const player = new Tone.Player({
+				url:
+					"https://tonejs.github.io/audio/berklee/gurgling_theremin_1.mp3",
+				loop: true,
+				autostart: true,
+			}).toDestination();
+			//create a distortion effect
+			// const distortion = new Tone.Distortion(0.4).toDestination();
+			//connect a player to the distortion
+			// player.connect(distortion);
+			await Tone.start();
+			Tone.loaded().then(() => {
+				player.start(player.now() + 0.01);
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+					<button onClick={this.play}>Play</button>
+					{/* <audio
+						id="ad"
+						controls
+						src="http://streaming.tdiradio.com:8000/house.mp3"
+					></audio> */}
+				</header>
+			</div>
+		);
+	}
 }
-
 export default App;
