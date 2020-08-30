62fa3ef288d5e98337dd6ae812f9709bffc3e421
leomet07 <leomet07@gmail.com>
2020-08-30 11:28:37 -0400

    Larger Size

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,8 @@ const app = express();
 
 app.use(cors()); // Use this after the variable declaration
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 
 app.use(helmet());
 app.use(morgan("tiny"));
